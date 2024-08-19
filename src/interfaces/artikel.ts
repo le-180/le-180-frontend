31643bc0fb0606352cb6fe0f61c799e967173739
bztke <bztke@systemli.org>
2024-08-19 18:53:31 +0200

    integrated strapi for articles and naive slug implementation

---
+++ src/interfaces/artikel.ts
@@ -0,0 +1,13 @@
+export default interface Artikel {
+  id: number;
+  attributes: {
+    Title: string;
+    These_Frage: string;
+    IsThese: boolean;
+    isFrage: boolean;
+    Text: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  };
+}
